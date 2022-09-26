42dee0fc4ed7ecc2a2473aa20d341ed43eb55342
AlenkinAndrew <oxqq@ya.ru>
2022-09-26 09:29:09 +0300

    api-gateway

---
+++ api-gateway/src/main/java/com/afalenkin/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,19 @@
+package com.afalenkin.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author Alenkin Andrew
+ * oxqq@ya.ru
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+}
